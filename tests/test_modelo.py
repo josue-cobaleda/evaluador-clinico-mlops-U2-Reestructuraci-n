949177906fcc9c6d00c9a8f59185782afaf59429
Sebastian Bonilla <sebastianbonilla92@hotmail.com>
2025-11-08 20:46:41 -0500

    Agrega pruebas unitarias y workflow

--- tests/test_modelo.py
+++ tests/test_modelo.py
@@ -1,6 +1,8 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app import app
 import os
 import requests
-from app import app
 
 def test_prediccion_leve():
     cliente = app.test_client()
